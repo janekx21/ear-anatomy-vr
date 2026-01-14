c3f0a284a140f3fd4d597355e2825fd89645b499
BeJunge <benjamin.junge@ovgu.de>
2026-01-14 17:02:09 +0100

    add animation, sound, wave;

--- Assets/SineWave.cs
+++ Assets/SineWave.cs
@@ -11,23 +11,40 @@ public class SineWave : MonoBehaviour
     [SerializeField]
     float speed =
     1.0f;
+    [SerializeField]
+    float height =
+1.0f;
     LineRenderer line;
-    
+
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    Transform source;
+
+    [SerializeField]
+    AudioClip clip;
+
     float progress = 0.0f;
+
+    float[] data;
     // Start is called before the first frame update
     void Start()
     {
         line = GetComponent<LineRenderer>();
         line.positionCount = 1000;
+
+        clip.GetData(data, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var dist = (target.position - source.position).magnitude;
         for (int i = 0; i < line.positionCount; i++)
         {
-            line.SetPosition(i, Vector3.right * i + Vector3.up * Mathf.Sin((float)i * sinScale + progress));
+            var t = (float)i / (float)line.positionCount;
+            
+            line.SetPosition(i, Vector3.Lerp(source.position, target.position, t) + Vector3.up * height * Mathf.Sin((float)i * sinScale* dist + progress));
 
         }
         progress += Time.deltaTime * speed;
